6d074645797d2847b2baf1db041a22e6482a19f3
Haley Young <haleyeyoung11@gmail.com>
2016-11-11 15:15:36 -0600

    Create header.php

---
+++ header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en-US">
+    <head>
+        <meta charset="utf-8">
+        <title>Haley E. Young</title>
+        <meta name="description" content="Haley Elizabeth Young is a web developer, designer, writer, student and more currently studying at the University of Wisconsin-Madison.">
+        <meta name="viewport" content="initial-scale=1.0, width=device-width">
+	<link rel="shortcut icon" type="image/jpeg" href="/img/Bee.jpg">
+        <link rel="stylesheet" type="text/css" href="main.css">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,300i,400,500">
+    </head>
+    <body>
+        <header>
+            <nav>
+                <a href="index.php"><img src="/img/Bee.svg" alt="Bee logo" class="logo-image">
+                <span class="logo">Haley E. Young</span></a>
+                <ul>
+                    <li><a href="about.php">About</a></li>
+                    <li><a href="portfolio.php">Portfolio</a></li>
+                    <li><a href="resume.php">Resume</a></li>
+                    <li><a href="contact.php">Contact</a></li>
+                </ul>
+            </nav>
+        </header>
