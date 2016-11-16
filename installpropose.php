30a5815059cb9e884d2ce77ae3ec4889f8628a6a
Haley Young <haleyeyoung11@gmail.com>
2016-11-16 12:20:51 -0600

    Create installpropose.php
    Adding a file to my portfolio section, showcasing my Install / Propose art project for Art 107 in the fall of 2016.

---
+++ installpropose.php
@@ -0,0 +1,13 @@
+<?php include('header.php');?>
+
+        <section id="design-engineering">
+            <h2 class="section-headline project">Design and Engineering</h2>
+            <h2 class="section-tagline">3D Modeling, Digital Design, Writing</h2>
+            <div class="inner-text project">
+                <p>In the fall of 2016, one of the projects in my digital foundations art class (an introductory course needed to pursue my graphic design certificate) was to create a proposal for an art exhibit in a real-world gallery space. The assignment was called <i>Install / Propose</i> and the learning objectives were to think creatively to come up with an interesting art installation, demonstrate competency with 3D modeling software to create images of your plan, and to use writing and design skills to bring everything together into one comprehensive proposal.</p>
+                <a href="img/installpropose.pdf" target="_blank"><img src="/img/installpropose.jpg" alt="A screenshot showing my completed proposal, linking to the full PDF version."></a>
+                <p>I used <a href="http://www.sketchup.com" target="_blank">SketchUp</a> to build my 3D design and Adobe Illustrator to create an aerial "plan view" of my proposal. I chose to center my proposed art exhibit around an intersection that is very important to me: that of design and engineering, specifically in how they relate to web development. I find immense satisfaction in reconciling these two activities and continually seek to understand how they work together to create a rich web experience.</p>
+            </div>
+        </section>
+
+<?php include('footer.php');?>
